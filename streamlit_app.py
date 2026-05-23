3b9f8cbba73837c1e5bdb781b06bbff4c47adb43
ikenurcahya740-commits <ikenurcahya740@gmail.com>
2026-05-23 08:32:51 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,8 @@ import streamlit as st
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+) import datetime
+import streamlit as st
+
+d = st.date_input("When's your birthday", datetime.date(2019, 7, 6))
+st.write("Your birthday is:", d)
