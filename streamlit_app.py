2b739f415f8c2a64c4226db21960ae22091aaed6
ikenurcahya740-commits <ikenurcahya740@gmail.com>
2026-05-23 08:40:03 +0700

    latihan1_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,3 +12,7 @@ st.badge("Success", icon=":material/check:", color="green")
 st.markdown(
     ":violet-badge[:material/star: Favorite] :orange-badge[⚠️ Needs review] :gray-badge[Deprecated]"
 )
+import streamlit as st
+
+st.caption("This is a string that explains something above.")
+st.caption("A caption with _italics_ :blue[colors] and emojis :sunglasses:")
