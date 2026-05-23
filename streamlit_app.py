86ac7cba85aa99a224aa91413dafd86f48b9c932
ikenurcahya740-commits <ikenurcahya740@gmail.com>
2026-05-23 08:36:58 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,11 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 ) 
-st write(
-    import datetime
 import streamlit as st
 
-d = st.date_input("When's your birthday", datetime.date(2019, 7, 6))
-st.write("Your birthday is:", d)
+st.badge("New")
+st.badge("Success", icon=":material/check:", color="green")
+
+st.markdown(
+    ":violet-badge[:material/star: Favorite] :orange-badge[⚠️ Needs review] :gray-badge[Deprecated]"
+)
