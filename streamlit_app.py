2b2cd5c6fa0859a17e76983601705fab19005951
ikenurcahya740-commits <ikenurcahya740@gmail.com>
2026-05-23 10:40:05 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -67,3 +67,13 @@ option = st.selectbox(
     "How would you like to be contacted?",
     ("Email", "Home phone", "Mobile phone"),
 )
+import streamlit as st
+
+option = st.selectbox(
+    "How would you like to be contacted?",
+    ("Email", "Home phone", "Mobile phone"),
+    index=None,
+    placeholder="Select contact method...",
+)
+
+st.write("You selected:", option)
